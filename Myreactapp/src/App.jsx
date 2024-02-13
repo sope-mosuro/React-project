e6005e3e5b5807569fca7562eb2f5dec9bc787d6
sope-mosuro <sopefrancis@gmail.com>
2024-02-13 17:33:00 +0100

    initialising project

---
+++ Myreactapp/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+
+function App() {
+ return(
+<>
+ <Header/>
+ <Footer/>
+ 
+</>
+ )
+
+ 
+   
+}
+
+export default App
---
+++ Myreactapp/src/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header(){
+ return(
+    <header>
+        <h1>My website</h1>
+        <nav><ul>
+            <li>home</li>
+            <li>other</li>
+            <li>nigga</li>
+            <li>yard</li>
+            </ul></nav>
+    </header>
+ )
+ 
+}
+
+export default Header
